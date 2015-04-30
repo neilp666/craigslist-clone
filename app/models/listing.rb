555098c899ae83038fa7161d4964ee2dcfcc9021
neil patel <neil@myindianweddingapp.com>
2015-04-30 10:56:12 +0100

    Added dropdown menu

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,6 @@
 class Listing < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
+
+  belongs_to :category
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :listings, except: [:index]
+  resources :categories, only: [:show]
 
   root 'listings#index'
 
